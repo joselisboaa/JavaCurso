083db6448e4e2e619af552f7240dd303f20b3182
1mmanuelKant <joselisboaa05@gmail.com>
2022-11-05 16:03:30 -0300

    feature: new formula

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -10,10 +10,38 @@ public class Triangulo {
         double ladoB = sc.nextDouble();
         double ladoC = sc.nextDouble();
 
-        double perimetro = semiPerimetroTotal(ladoA, ladoB, ladoC);
+        System.out.println("Lados do triângulo Y");
+
+        double ladoX = sc.nextDouble();
+        double ladoY = sc.nextDouble();
+        double ladoZ = sc.nextDouble();
+
+        sc.close();
+        //Triângulo X
+        double semiPerimetroX = semiPerimetroTotal(ladoA, ladoB, ladoC);
+        double areaX = areaTotal(semiPerimetroX, ladoA, ladoB, ladoC);
+        System.out.println("A área do Triângulo X é igual a:" + areaX);
+        //Triângulo Y
+        double semiPerimetroY = semiPerimetroTotal(ladoX, ladoY, ladoZ);
+        double areaY = areaTotal(semiPerimetroY, ladoX, ladoY, ladoZ);
+        System.out.println("A área do Triângulo Y é igual a:" + areaY);
+
+        System.out.println("A maior área é do triângulo: " + comparadorAreas(areaX, areaY));
+    }
+
+    private static String comparadorAreas (double areaX, double areaY) {
+        if(areaX > areaY) {
+            return "TrianguloX";
+        } else {
+            return "TrianguloY";
+        }
+    }
+
+    private static double areaTotal(double p, double a, double b, double c) {
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     private static double semiPerimetroTotal(double a, double b, double c ) {
-        return (a + b + c)/2;
+        return (a + b + c) / 2;
     }
 }
