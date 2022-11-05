fcee267627801827c7c937da571d5e622af60b0d
1mmanuelKant <joselisboaa05@gmail.com>
2022-11-05 17:02:51 -0300

    using a constructor

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -2,28 +2,38 @@ import java.util.Scanner;
 
 public class Triangulo {
 
+    private double ladoA;
+    private double ladoB;
+    private double ladoC;
+
+    public Triangulo(double ladoA, double ladoB, double ladoC) {
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.ladoC = ladoC;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Lados do triângulo X");
+        Triangulo x, y;
 
-        double ladoA = sc.nextDouble();
-        double ladoB = sc.nextDouble();
-        double ladoC = sc.nextDouble();
+        System.out.println("Lados do triângulo X");
+        x = new Triangulo(sc.nextDouble(), sc.nextDouble(), sc.nextDouble());
 
         System.out.println("Lados do triângulo Y");
-
-        double ladoX = sc.nextDouble();
-        double ladoY = sc.nextDouble();
-        double ladoZ = sc.nextDouble();
-
+        y = new Triangulo(sc.nextDouble(), sc.nextDouble(), sc.nextDouble());
         sc.close();
+
+        System.out.println(x.ladoA);
+        System.out.println(y.ladoB);
+
         //Triângulo X
-        double semiPerimetroX = semiPerimetroTotal(ladoA, ladoB, ladoC);
-        double areaX = areaTotal(semiPerimetroX, ladoA, ladoB, ladoC);
+        double semiPerimetroX = semiPerimetroTotal(x.ladoA, x.ladoB, x.ladoC);
+        double areaX = areaTotal(semiPerimetroX, x.ladoA, x.ladoB, x.ladoC);
         System.out.println("A área do Triângulo X é igual a:" + areaX);
+
         //Triângulo Y
-        double semiPerimetroY = semiPerimetroTotal(ladoX, ladoY, ladoZ);
-        double areaY = areaTotal(semiPerimetroY, ladoX, ladoY, ladoZ);
+        double semiPerimetroY = semiPerimetroTotal(y.ladoA, y.ladoB, y.ladoC);
+        double areaY = areaTotal(semiPerimetroY, y.ladoA, y.ladoB, y.ladoC);
         System.out.println("A área do Triângulo Y é igual a:" + areaY);
 
         System.out.println("A maior área é do triângulo: " + comparadorAreas(areaX, areaY));
