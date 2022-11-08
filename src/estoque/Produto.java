58ff2ba8cf4ce6e20c1df1d86cda3188c983ab67
1mmanuelKant <joselisboaa05@gmail.com>
2022-11-07 22:01:32 -0300

    first commit

---
+++ src/estoque/Produto.java
@@ -0,0 +1,39 @@
+package estoque;
+
+import entities.Product;
+
+import java.util.Scanner;
+
+public class Produto {
+
+
+ public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    Product produto = new Product();
+    System.out.println("Name: ");
+    produto.name = sc.next();
+    System.out.println("Quantity: ");
+    produto.price = sc.nextDouble();
+    System.out.println("Quantity in stock");
+    produto.quantity = sc.nextInt();
+
+    System.out.println("Product data: " + produto.name + ", $" + produto.price + ", " + produto.quantity + " units, ");
+
+    System.out.println("Total: $ " + produto.totalValueStock());
+
+    System.out.println("Enter a number of products to be add in stock: ");
+    produto.addProducts(sc.nextInt());
+
+    System.out.println("Updated data: " + produto.name + ", $" + produto.price + ", " + produto.quantity + " units, ");
+
+    System.out.println("Total: $ " + produto.totalValueStock());
+
+    System.out.println("Enter the number of products to be removed from stock: ");
+
+    produto.removeProducts(sc.nextInt());
+    System.out.println("Updated data: " + produto.name + ", $" + produto.price + ", " + produto.quantity + " units, ");
+
+ }
+}
